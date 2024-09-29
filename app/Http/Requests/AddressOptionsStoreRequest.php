3d7f09065cbbf5b568c39a3e96a3ea1466ad0436
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2024-09-29 22:48:52 +0530

    !! validating $request using "AddressOptionsStoreRequest" !!

--- app/Http/Requests/AddressOptionsStoreRequest.php
+++ app/Http/Requests/AddressOptionsStoreRequest.php
@@ -7,14 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class AddressOptionsStoreRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
@@ -22,7 +14,13 @@ class AddressOptionsStoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'country' => 'required',
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'addr1' => 'required',
+            'addr2' => 'required',
+            'city' => 'required',
+            'postcode' => 'required',
         ];
     }
 }
