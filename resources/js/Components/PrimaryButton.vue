340b922b535fb926c51e0a7984080476557785aa
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2024-09-29 10:28:48 +0530

    "PrimaryButton" component styles and color changed.

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -1,6 +1,18 @@
 <template>
     <button
-        class="inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900"
+        class="
+            w-full
+            text-center
+            items-center
+            px-4
+            py-1.5
+            bg-[#fcba1f]
+            border border-gray-600
+            rounded-sm
+            text-sm
+            font-extrabold
+            text-black
+        "
     >
         <slot />
     </button>
