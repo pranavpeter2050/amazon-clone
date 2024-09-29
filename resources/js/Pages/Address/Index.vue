4a67f54af5d7ac50b71339d5c1cd99c0ff672193
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2024-09-29 22:53:41 +0530

    updated Address/Index.vue file.

--- resources/js/Pages/Address/Index.vue
+++ resources/js/Pages/Address/Index.vue
@@ -10,7 +10,7 @@
 
     <div class="max-w-[1000px] mx-auto flex gap-2 h-[270px]">
       <div class="border border-dotted border-gray-400 rounded-md w-1/3">
-        <Link v-if="show" href="/">
+        <Link v-if="!$page.props.auth.address" :href="route('address_options.index')">
           <div class="grid h-full place-items-center cursor-pointer">
             <div class="text-center">
               <div class="flex justify-center">
@@ -28,7 +28,7 @@
         </div>
       </div>
 
-      <div class="relative border border-gray-400 rounded-md w-1/3 shadow-md">
+      <div v-if="$page.props.auth.address" class="relative border border-gray-400 rounded-md w-1/3 shadow-md">
         <div class="flex items-center justify-start p-3 text-xs text-gray-600 font-extrabold border-b border-b-gray-400">
           Default: <img class="h-3 mt-1.5 ml-2" src="/images/logo/AMAZON_LOGO_DARK.png" alt="">
         </div>
@@ -63,7 +63,6 @@ import { Head, Link } from '@inertiajs/vue3';
 import { toRefs } from 'vue';
 import MapMarkerOutlineIcon from 'vue-material-design-icons/MapMarkerOutline.vue';
 import PlusIcon from 'vue-material-design-icons/Plus.vue';
-let show = true
 
 const props = defineProps({ product: Object });
 const { product } = toRefs(props);
