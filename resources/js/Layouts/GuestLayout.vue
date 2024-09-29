78733c30d8f2f32e5dbaa9a7e0e01bb396ad5144
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2024-09-29 10:30:03 +0530

    "GuestLayout" (baselayout for login/register) changed.

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -5,18 +5,27 @@ import { Link } from '@inertiajs/vue3';
 
 <template>
     <div
-        class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0"
+        class="flex min-h-screen flex-col items-center pt-6"
     >
         <div>
             <Link href="/">
-                <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
+                <img class="h-12 fill-current text-gray-500" src="/images/logo/AMAZON_LOGO_DARK.png" alt="">
             </Link>
         </div>
 
         <div
-            class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg"
+            class="mt-6 w-full overflow-hidden bg-white px-6 py-4 border border-gray-300 max-w-[400px] rounded-sm"
         >
             <slot />
+        </div>
+
+        <div v-if="$page.url === '/login'" class="w-[400px]">
+            <div class="text-center text-sm text-gray-600 font-bold py-4">New to Amazon</div>
+            <div class="w-full border border-gray-500 bg-gray-200 text-sm font-extrabold p-2 hover:bg-gray-300 text-center">
+                <Link :href="route('register')">
+                    Create your Amazon account
+                </Link>
+            </div>
         </div>
     </div>
 </template>
