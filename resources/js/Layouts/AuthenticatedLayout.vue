09596831956f85a64531a5bcb9dca0dc8e9a9cc5
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2024-09-29 09:02:05 +0530

    added slideInLeft-menu

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -5,9 +5,12 @@ import MenuDownIcon from 'vue-material-design-icons/MenuDown.vue';
 import MagnifyIcon from 'vue-material-design-icons/Magnify.vue';
 import CartMinusIcon from 'vue-material-design-icons/CartMinus.vue';
 import MenuIcon from 'vue-material-design-icons/Menu.vue';
+import AccountCircleIcon from 'vue-material-design-icons/AccountCircle.vue';
+import CloseIcon from 'vue-material-design-icons/Close.vue';
+import ChevronRightIcon from 'vue-material-design-icons/ChevronRight.vue';
 import { Link } from '@inertiajs/vue3';
 
-const showingNavigationDropdown = ref(false);
+let showMenu = ref(false);
 </script>
 
 <template>
@@ -104,7 +107,7 @@ const showingNavigationDropdown = ref(false);
         <!-- sub-navigation -->
         <div class="flex items-center justify-between bg-[#232f3e] h-[38px] fixed z-40 min-w-[1150px] w-full mt-[60px]">
             <div class="flex">
-                <div class="flex h-[30px] border-[1px] border-[#232f3e] rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer">
+                <div @click="showMenu = true" class="flex h-[30px] border-[1px] border-[#232f3e] rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer">
                     <div class="flex items-center justify-between px-2">
                         <MenuIcon fillColor="#f5f5f5" :size="26" class="mr-0.5" />
                         <div class="text-[14px] text-white font-extrabold">All</div>
@@ -218,5 +221,38 @@ const showingNavigationDropdown = ref(false);
                 </ul>
             </div>
         </footer>
+    </div>
+
+    <div
+        v-if="showMenu" 
+        class="top-0 z-50 fixed w-full h-full bg-black bg-opacity-70"
+        :class="[showMenu ? 'animate__animated animate__fadeIn animate__faster' : '']"
+    >
+        <CloseIcon 
+            @click="showMenu = false"
+            :size="30"
+            fillColor="#dcdcdc"
+            class="ml-2.5 mt-2 left-80 cursor-pointer fixed z-50"
+            :class="[showMenu ? 'animate__animated animate__fadeIn animate__faster' : '']"
+        />
+
+        <div 
+            class="w-80 h-full bg-white"
+            :class="[showMenu ? 'animate__animated animate__slideInLeft animate__faster' : '']"
+        >
+            <div class="bg-[#232f3e] font-extrabold text-[18px] flex items-center p-2 text-white pl-7">
+                <span>Hello, Sign in</span>
+            </div>
+
+            <div class="font-extrabold text-[16px] pt-3 pb-1 pl-6 pr-3 text-black">
+                Shop by Department
+            </div>
+
+            <div class="hover:bg-gray-200 pl-6 pr-3">
+                <div class="py-2.5 text-[13px] text-black flex justify-between items-center hover:bg-gray-200 cursor-pointer">
+                    Computers <ChevronRightIcon :size="20" fillColor="#808080" />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
