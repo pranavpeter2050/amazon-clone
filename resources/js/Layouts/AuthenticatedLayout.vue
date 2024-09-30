ef903d4d5717c102946a8f077499abdb95588d3b
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2024-10-01 01:21:22 +0530

    created "Cart" page.

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -184,14 +184,14 @@ const accountAndListFunc = (bool) => {
                     </div>
                 </div>
 
-                <div class="relative h-[50px] p-2 border-[1px] border-gray-900 rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer">
+                <Link :href="route('cart.index')" class="relative h-[50px] p-2 border-[1px] border-gray-900 rounded-sm hover:border-[1px] hover:border-gray-100 cursor-pointer">
                     <span class="absolute text-center right-[21px] w-[14px] -top-0 rounded-full text-[20px]">
                         <div class="text-orange-400 font-extrabold bg-gray-900 h-[16px]">{{ cartStore.cart.length }}</div>
                     </span>
                     <div class="flex items-center justify-center">
                         <CartMinusIcon fillColor="#fcfcfc" :size="40" class="-mt-0.5" />
                     </div>
-                </div>
+                </Link>
             </div>
 
         </div>
